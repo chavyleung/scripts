3a766b4f9aa71e22f9233ad500d620720afab3f4
Chavy <chavyleung@gmail.com>
2019-12-27 23:21:09 +0800

    修复cookieKey找不到问题

--- neteasemusic/quanx/neteasemusic.js
+++ neteasemusic/quanx/neteasemusic.js
@@ -12,46 +12,42 @@
  */
 
 function sign() {
-  const cookieName = "网易云音乐";
-  const ccookieKey = "chavy_cookie_neteasemusic";
-  const cookieVal = $prefs.valueForKey(cookieKey);
+  const cookieName = '网易云音乐'
+  const cookieKey = 'chavy_cookie_neteasemusic'
+  const cookieVal = $prefs.valueForKey(cookieKey)
 
-  const pc = `http://music.163.com/api/point/dailyTask?type=1`;
-  const mobile = `http://music.163.com/api/point/dailyTask?type=0`;
+  const pc = `http://music.163.com/api/point/dailyTask?type=1`
+  const mobile = `http://music.163.com/api/point/dailyTask?type=0`
 
   let url = {
     url: null,
     headers: {
       Cookie: cookieVal
     }
-  };
-  url.url = pc;
-  $task.fetch(url).then(response => {
-    log("网易云音乐(PC端)", response.body);
-  });
+  }
+  url.url = pc
+  $task.fetch(url).then((response) => {
+    log('网易云音乐(PC端)', response.body)
+  })
 
-  url.url = mobile;
-  $task.fetch(url).then(response => {
-    log("网易云音乐(移动端)", response.body);
-  });
+  url.url = mobile
+  $task.fetch(url).then((response) => {
+    log('网易云音乐(移动端)', response.body)
+  })
 }
 
 function log(title, data) {
-  let result = JSON.parse(data);
+  let result = JSON.parse(data)
   if (result.code == 200) {
-    console.log(`签到成功: ${title}`);
-    $notify("签到成功", title, "");
+    console.log(`签到成功: ${title}`)
+    $notify('签到成功', title, '')
   } else if (result.code == -2) {
-    console.log(
-      `签到跳过: ${title}, 编码: ${result.code}, 原因: ${result.msg}`
-    );
-    $notify("签到跳过", title, `原因: ${result.msg}`);
+    console.log(`签到跳过: ${title}, 编码: ${result.code}, 原因: ${result.msg}`)
+    $notify('签到跳过', title, `原因: ${result.msg}`)
   } else {
-    console.log(
-      `签到失败: ${title}, 编码: ${result.code}, 原因: ${result.msg}`
-    );
-    $notify("签到失败", title, `原因: ${result.msg}`);
+    console.log(`签到失败: ${title}, 编码: ${result.code}, 原因: ${result.msg}`)
+    $notify('签到失败', title, `原因: ${result.msg}`)
   }
 }
 
-sign();
+sign()
