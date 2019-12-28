b87791b547bf6cf598b2a1624c92846a7ef47329
Chavy <chavyleung@gmail.com>
2019-12-28 13:52:40 +0800

    更新说明文件: 笔误

---
+++ tieba/quanx/tieba.js
@@ -0,0 +1,37 @@
+const cookieName = '百度贴吧'
+const cookieKey = 'chavy_cookie_tieba'
+const cookieVal = $prefs.valueForKey(cookieKey)
+
+function sign() {
+  let url = {
+    url: `http://tieba.baidu.com/f/like/mylike`,
+    headers: {
+      Cookie: cookieVal
+    }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    let regex = /\/f\?kw=([^{'"}]*)/g
+    for (const bar of data.matchAll(regex)) {
+      signBar(bar)
+    }
+  })
+}
+
+function signBar(bar) {
+  let url = {
+    url: `http://tieba.baidu.com/sign/add?ie=utf-8&kw=${bar[1]}`,
+    headers: { Cookie: cookieVal }
+  }
+  $task.fetch(url).then((response) => {
+    let data = response.body
+    let result = JSON.parse(data)
+    if (result.no == 0) {
+      console.log(`正在签到: ${bar[1]}, 签到成功`)
+    } else {
+      console.log(`正在签到: ${bar[1]}, 错误编码: ${result.no}, 错误原因: ${result.error}`)
+    }
+  })
+}
+
+sign()
