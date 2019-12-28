f06e6cd2e3a6c3da5b907ba28a0730cdc0acb74a
Chavy <chavyleung@gmail.com>
2019-12-28 14:20:36 +0800

    quanx - 重新上传

--- tieba/quanx/tieba.js
+++ tieba/quanx/tieba.js
@@ -1,6 +1,6 @@
-const cookieName = '百度贴吧'
-const cookieKey = 'chavy_cookie_tieba'
-const cookieVal = $prefs.valueForKey(cookieKey)
+const cookieName = "百度贴吧";
+const cookieKey = "chavy_cookie_tieba";
+const cookieVal = $prefs.valueForKey(cookieKey);
 
 function sign() {
   let url = {
@@ -8,30 +8,35 @@ function sign() {
     headers: {
       Cookie: cookieVal
     }
-  }
-  $task.fetch(url).then((response) => {
-    let data = response.body
-    let regex = /\/f\?kw=([^{'"}]*)/g
+  };
+  $task.fetch(url).then(response => {
+    let data = response.body;
+    let regex = /\/f\?kw=([^{'"}]*)/g;
+    let cnt = 0;
     for (const bar of data.matchAll(regex)) {
-      signBar(bar)
+      cnt += 1;
+      setTimeout(() => signBar(bar), cnt * 500);
     }
-  })
+  });
 }
 
 function signBar(bar) {
   let url = {
     url: `http://tieba.baidu.com/sign/add?ie=utf-8&kw=${bar[1]}`,
+    method: "POST",
     headers: { Cookie: cookieVal }
-  }
-  $task.fetch(url).then((response) => {
-    let data = response.body
-    let result = JSON.parse(data)
+  };
+  $task.fetch(url).then(response => {
+    let data = response.body;
+    let result = JSON.parse(data);
     if (result.no == 0) {
-      console.log(`正在签到: ${bar[1]}, 签到成功`)
+      console.log(`正在签到: ${bar[1]}, 签到成功`);
     } else {
-      console.log(`正在签到: ${bar[1]}, 错误编码: ${result.no}, 错误原因: ${result.error}`)
+      console.log(
+        `正在签到: ${bar[1]}, 错误编码: ${result.no}, 错误原因: ${result.error}`
+      );
     }
-  })
+  });
 }
 
-sign()
+sign();
