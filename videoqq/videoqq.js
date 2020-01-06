c9ec8f6c35b42eb7c742ca61f5d138680dddfd38
Chavy <chavyleung@gmail.com>
2020-01-06 19:41:19 +0800

    更新[腾讯视频]签到脚本 (beta): 优化通知

--- videoqq/videoqq.js
+++ videoqq/videoqq.js
@@ -17,8 +17,7 @@ function sign() {
     let subTitle = ''
     let detail = ''
     if (result.ret == 0) {
-      subTitle = '签到结果: 成功'
-      chavy.msg(title, subTitle, detail)
+      getexp()
     } else if (result.ret == -10006) {
       subTitle = '签到结果: 失败'
       detail = `原因: 未登录, 说明: ${result.msg}`
@@ -33,8 +32,21 @@ function sign() {
       chavy.msg(title, subTitle, detail)
     }
   })
-
   chavy.done()
+}
+
+function getexp() {
+  const timestamp = Date.parse(new Date())
+  let url = { url: `https://vip.video.qq.com/fcgi-bin/comm_cgi?name=spp_PropertyNum&cmd=1&growth_value=1&otype=json&_=${timestamp}`, headers: { Cookie: cookieVal } }
+  url.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15'
+  chavy.get(url, (error, response, data) => {
+    chavy.log(`${cookieName}, data: ${data}`)
+    let result = JSON.parse(data.match(/QZOutputJson=\(([^\)]*)\)/)[1])
+    const title = `${cookieName}`
+    let subTitle = '签到结果: 成功'
+    let detail = `V力值: ${result.GrowthValue.num}, 观影券: ${result.MovieTicket.num}, 赠片资格: ${result.GiveMovie.num}`
+    chavy.msg(title, subTitle, detail)
+  })
 }
 
 function init() {
