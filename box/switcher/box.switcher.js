29e5f788064728e8b27cfcbe80fe111c6cfbeb4b
Chavy <chavyleung@gmail.com>
2020-06-08 10:35:03 +0800

    添加[box.switcher.js]: 定时运行 switcher.js 即可自动切换会话

---
+++ box/switcher/box.switcher.js
@@ -0,0 +1,73 @@
+const $ = new Env('会话切换')
+$.KEY_sessions = 'chavy_boxjs_sessions'
+
+!(async () => {
+  $.log('', `🔔 ${$.name}, 开始!`, '')
+  await execSwitch()
+})()
+  .catch((e) => {
+    $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+  })
+  .finally(() => {
+    $.msg($.name, $.subt, $.desc), $.log('', `🔔 ${$.name}, 结束!`, ''), $.done()
+  })
+
+function execSwitch() {
+  return new Promise((resove) => {
+    // 会话排序: `创建时间`升序
+    const sessions = getSessions()
+    sessions.sort((a, b) => a.createTime.replace(/-|:| /g, '') - b.createTime.replace(/-|:| /g, ''))
+    const apps = {}
+    sessions.forEach((session) => {
+      const appId = session.appId
+      const appName = session.appName
+      apps[appId] = apps[appId] ? apps[appId] : { id: appId, name: appName, sessions: [], skipedSessions: [] }
+      const app = apps[appId]
+      session.datas.sort((a, b) => a.key.localeCompare(b.key))
+      const appDatas = []
+      session.datas.forEach((data) => appDatas.push({ key: data.key, val: $.getdata(data.key) }))
+      // 会话列表自重复
+      const duplicateSession = app.sessions.find((_session) => JSON.stringify(_session.datas) === JSON.stringify(session.datas))
+      if (duplicateSession) {
+        $.log(`⚠️ ${$.name}, ${appName}: ${session.name} 与 ${duplicateSession.name} 重复, 建议删除!`)
+        app.skipedSessions.push(session)
+        return true
+      }
+      // 会话为当前会话
+      else if (JSON.stringify(session.datas) === JSON.stringify(appDatas)) {
+        app.sessions.push(session)
+        app.curSessionId = session.id
+        return true
+      }
+      // 继续添加会话
+      else {
+        app.sessions.push(session)
+      }
+    })
+    Object.keys(apps).forEach((appId) => {
+      const app = apps[appId]
+      if (app.sessions.length <= 1) {
+        $.log(`⚠️ ${$.name}, ${app.name}: 跳过!`)
+        return true
+      }
+      const curSessionIdx = app.sessions.findIndex((session) => session.id === app.curSessionId)
+      const curSession = app.sessions[curSessionIdx]
+      const isNewRound = curSessionIdx + 1 === app.sessions.length
+      const nextSessionIdx = isNewRound ? 0 : curSessionIdx + 1
+      const nextSession = app.sessions[nextSessionIdx]
+      nextSession.datas.forEach((_data) => $.setdata(_data.val, _data.key) || true)
+      $.log(`❕ ${$.name}, ${curSession.appName}: ${curSession.name} => ${nextSession.name} ${isNewRound ? '(新一轮)' : ''}`)
+    })
+
+    resove()
+  })
+}
+
+function getSessions() {
+  const sessionstr = $.getdata($.KEY_sessions)
+  const sessions = sessionstr ? JSON.parse(sessionstr) : []
+  return Array.isArray(sessions) ? sessions : []
+}
+
+// prettier-ignore
+function Env(t){this.name=t,this.logs=[],this.isSurge=(()=>"undefined"!=typeof $httpClient),this.isQuanX=(()=>"undefined"!=typeof $task),this.log=((...t)=>{this.logs=[...this.logs,...t],t?console.log(t.join("\n")):console.log(this.logs.join("\n"))}),this.msg=((t=this.name,s="",i="")=>{this.isSurge()&&$notification.post(t,s,i),this.isQuanX()&&$notify(t,s,i),this.log("==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="),t&&this.log(t),s&&this.log(s),i&&this.log(i)}),this.getdata=(t=>this.isSurge()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):void 0),this.setdata=((t,s)=>this.isSurge()?$persistentStore.write(t,s):this.isQuanX()?$prefs.setValueForKey(t,s):void 0),this.get=((t,s)=>this.send(t,"GET",s)),this.wait=((t,s=t)=>i=>setTimeout(()=>i(),Math.floor(Math.random()*(s-t+1)+t))),this.post=((t,s)=>this.send(t,"POST",s)),this.send=((t,s,i)=>{if(this.isSurge()){const e="POST"==s?$httpClient.post:$httpClient.get;e(t,(t,s,e)=>{s.body=e,s.statusCode=s.status,i(t,s,e)})}this.isQuanX()&&(t.method=s,$task.fetch(t).then(t=>{t.status=t.statusCode,i(null,t,t.body)},t=>i(t.error,t,t)))}),this.done=((t={})=>$done(t))}
--- chavy.box.js
+++ chavy.box.js
@@ -46,6 +46,17 @@ function getPath(url) {
   return new RegExp($.domain).test(url) ? fullpath.split($.domain)[1] : undefined
 }
 
+function getSystemCfgs() {
+  return {
+    env: $.isSurge() ? 'Surge' : $.isQuanX ? 'QuanX' : 'Loon',
+    envs: [
+      { id: 'Surge', icon: 'https://is3-ssl.mzstatic.com/image/thumb/Purple123/v4/21/0a/0d/210a0df9-cbe1-b9dc-7549-4c10cd996279/AppIcon-0-0-1x_U007emarketing-0-0-0-6-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/492x0w.png' },
+      { id: 'QuanX', icon: 'https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/ae/7c/a2/ae7ca2fb-c4b6-d3a9-885a-935c1ea34cdb/AppIcon-1x_U007emarketing-0-7-0-0-85-220.png/492x0w.png' },
+      { id: 'Loon', icon: 'https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/59/2c/fb/592cfb3b-162e-83dc-4e96-7a2175e1d29f/AppIcon-0-1x_U007emarketing-0-7-0-0-85-220.png/492x0w.png' }
+    ]
+  }
+}
+
 function getSystemApps() {
   const sysapps = [
     {
@@ -71,6 +82,17 @@ function getSystemApps() {
       author: '@chavyleung',
       repo: 'https://github.com/chavyleung/scripts/tree/master/acfun',
       icon: 'https://is5-ssl.mzstatic.com/image/thumb/Purple123/v4/41/f0/11/41f011f8-1bbc-188f-220f-3db5164a9ae9/AppIcon-1x_U007emarketing-0-7-0-0-85-220.png/492x0w.png'
+    },
+    {
+      id: 'ApkTw',
+      name: 'ApkTw',
+      keys: ['chavy_cookie_apktw'],
+      author: '@chavyleung',
+      repo: 'https://github.com/chavyleung/scripts/tree/master/apktw',
+      url: 'https://apk.tw/',
+      icon: 'https://raw.githubusercontent.com/Orz-3/mini/master/apktw.png',
+      tasks: [{ cron: '3 0 * * *', script: 'noteyoudao.js' }],
+      rewrites: [{ type: 'request', pattern: '^https://note.youdao.com/yws/mapi/user?method=checkin', script: 'noteyoudao.cookie.js', body: true }]
     },
     {
       id: 'BAIDU',
@@ -119,6 +141,17 @@ function getSystemApps() {
       author: '@chavyleung',
       repo: 'https://github.com/chavyleung/scripts/tree/master/v2ex',
       icon: 'https://raw.githubusercontent.com/Orz-3/mini/master/v2ex.png'
+    },
+    {
+      id: 'NoteYoudao',
+      name: '有道云笔记',
+      keys: ['chavy_signurl_noteyoudao', 'chavy_signbody_noteyoudao', 'chavy_signheaders_noteyoudao'],
+      author: '@chavyleung',
+      repo: 'https://github.com/chavyleung/scripts/tree/master/noteyoudao',
+      url: 'https://apps.apple.com/cn/app/有道云笔记-扫描王版/id450748070',
+      icon: 'https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/25/6a/00/256a002d-b5f0-46e1-ef55-841d41f8aafc/AppIcon-0-1x_U007emarketing-0-7-0-0-85-220.png/460x0w.png',
+      tasks: [{ cron: '3 0 * * *', script: 'noteyoudao.js' }],
+      rewrites: [{ type: 'request', pattern: '^https://note.youdao.com/yws/mapi/user?method=checkin', script: 'noteyoudao.cookie.js', body: true }]
     }
   ]
   sysapps.forEach((app) => {
@@ -209,6 +242,7 @@ function handleHome() {
     sessions: getSessions(),
     sysapps: getSystemApps(),
     userapps: getUserApps(),
+    syscfgs: getSystemCfgs(),
     colors: getSystemThemes()
   })
   $.html = `
@@ -225,13 +259,28 @@ function handleHome() {
       <div id="app">
         <v-app>
           <v-app-bar :color="ui.appbar.color" app dense>
-            <v-menu bottom left v-if="['app', 'home', 'log', 'data'].includes(ui.curview)">
+            <v-menu bottom left v-if="['app', 'home', 'log', 'data'].includes(ui.curview) && box.syscfgs.env === ''">
               <template v-slot:activator="{ on }">
                 <v-btn icon v-on="on"><v-icon>mdi-palette</v-icon></v-btn>
               </template>
               <v-list>
                 <v-list-item v-for="(color, colorIdx) in box.colors" :key="color.id" @click="ui.appbar.color=color.id">
-                  <v-list-item-title>{{ color.name }}</v-list-item-title>
+                  <v-list-item-title>22{{ color.name }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <v-menu bottom left v-else-if="['app', 'home', 'log', 'data'].includes(ui.curview) && box.syscfgs.env !== ''">
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-avatar size="24">
+                    <img :src="box.syscfgs.envs.find(e=>e.id===box.syscfgs.env).icon" alt="box.syscfgs.env" />
+                  </v-avatar>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item v-for="(env, envIdx) in box.syscfgs.envs" :key="env.id" @click="box.syscfgs.env=env.id">
+                  <v-list-item-avatar size="24"><v-img :src="env.icon"></v-img></v-list-item-avatar>
+                  <v-list-item-title>{{ env.id }}</v-list-item-title>
                 </v-list-item>
               </v-list>
             </v-menu>
