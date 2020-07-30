268932945f35aa087808e3280216de34f4907e44
Chavy <chavyleung@gmail.com>
2020-07-30 22:02:32 +0800

    更新[EnvJs]: 修复一处逻辑错误

--- Env.js
+++ Env.js
@@ -195,8 +195,8 @@ function Env(name, opts) {
           if (!err && resp) {
             resp.body = body
             resp.statusCode = resp.status
-            callback(err, resp, body)
           }
+          callback(err, resp, body)
         })
       } else if (this.isQuanX()) {
         $task.fetch(opts).then(
@@ -341,7 +341,7 @@ function Env(name, opts) {
     logErr(err, msg) {
       const isPrintSack = !this.isSurge() && !this.isQuanX() && !this.isLoon()
       if (!isPrintSack) {
-        $.log('', `❗️${this.name}, 错误!`, err.message)
+        $.log('', `❗️${this.name}, 错误!`, err)
       } else {
         $.log('', `❗️${this.name}, 错误!`, err.stack)
       }
