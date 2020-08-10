6418556dfbf302d7d3f7f89c70d1c13c56555751
QQ515237330 <yangtingxiao@gmail.com>
2020-08-10 11:01:09 +0800

    更新[中国电信] 修复没有获取金币body时 没有通知的问题

--- 10000/10000.js
+++ 10000/10000.js
@@ -81,7 +81,11 @@ function gethomeinfo() {
   const homebody = $.getdata($.KEY_signbody)
 	const url = { url: 'https://wapside.189.cn:9001/api/home/homeInfo', body : homebody, headers: {} }
   url.headers['Content-Type'] = 'application/json;charset=UTF-8'
-	if (!homebody) return
+	if (!homebody)
+        {
+            resolve()
+            return;
+        }
 	$.post(url, (err, resp, data) => {
 		   try {
 		       const _data = JSON.parse(data);
