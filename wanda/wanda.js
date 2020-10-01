181cf3a65130798a3d61921a22a476f93e50538c
GideonSenku <39037656+GideonSenku@users.noreply.github.com>
2020-10-01 13:34:13 +0800

    更新[万达电影]，10月签到活动码

--- wanda/wanda.js
+++ wanda/wanda.js
@@ -1,7 +1,7 @@
 const $ = new Env('万达电影')
 $._mi_ = 'senku_wanda_mi_'
 $.desc = []
-$.signCode = '73789488'
+$.signCode = '95791112'
 $.questionCode = '15884027'
 const date = tTime(new Date().getTime())
 
