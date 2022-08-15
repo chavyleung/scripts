40adf6238dc9237ef25b605229fea5f4e15d0329
Chavy <chavyleung@gmail.com>
2022-08-15 10:10:22 +0800

    feat(emby): add emby unlock script

---
+++ emby/emby.js
@@ -0,0 +1,20 @@
+const CHECK_URL = 'mb3admin.com/admin/service/registration/validateDevice'
+
+const url = $request.url
+const isCheckURL = (url) => url.includes(CHECK_URL)
+
+if (isCheckUrl(url) && $response.status != 200) {
+  const unlock = {
+    cacheExpirationDays: 999,
+    resultCode: 'GOOD',
+    message: 'Device Valid'
+  }
+
+  const status = 200
+  const headers = $response.headers
+  const body = JSON.stringify(unlock)
+
+  $done({ status, headers, body })
+} else {
+  $done({})
+}
