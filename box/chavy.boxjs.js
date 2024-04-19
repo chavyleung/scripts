fee01128519c573c836c0e21d53f4986ffd81f40
Chavy <chavyleung@gmail.com>
2024-04-19 15:21:12 +0800

    fix(boxjs): 数据编辑器无法保存数据问题

--- box/chavy.boxjs.js
+++ box/chavy.boxjs.js
@@ -3,7 +3,7 @@ const $ = new Env('BoxJs')
 // 为 eval 准备的上下文环境
 const $eval_env = {}
 
-$.version = '0.18.0'
+$.version = '0.18.1'
 $.versionType = 'beta'
 
 // 发出的请求需要需要 Surge、QuanX 的 rewrite
@@ -312,7 +312,7 @@ async function handleOptions() {}
 
 function getBoxData() {
   const datas = {}
-  
+
   const extraDatas =
     $.getdata(`${$.KEY_usercfgs.replace('#', '@')}.gist_cache_key`) || []
 
@@ -571,6 +571,8 @@ function getSystemApps() {
  */
 function getUserCfgs() {
   const defcfgs = {
+    gist_cache_key: [],
+
     favapps: [],
     appsubs: [],
     viewkeys: [],
